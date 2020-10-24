59266a2713c3619bce142d2f40850b14cfeb4dc6
Kanisk <57884127+chakrakan@users.noreply.github.com>
2020-10-23 23:40:15 -0400

    add: modal setup

---
+++ src/assets/icons/close-icon.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const CloseIconWrapper = styled.svg`
+  width: 100%;
+  height: 100%;
+`;
+
+export const CloseIcon = () => {
+  <CloseIconWrapper aria-hidden="true">
+    <path d="M-496.6-203l16.7,16.7c1.2,1.2,1.2,3.2,0,4.4c-1.2,1.2-3.2,1.2-4.4,0l-16.7-16.7l-16.7,16.7   c-1.2,1.2-3.2,1.2-4.4,0c-1.2-1.2-1.2-3.2,0-4.4l16.7-16.7l-16.7-16.7c-1.2-1.2-1.2-3.2,0-4.4c1.2-1.2,3.2-1.2,4.4,0l16.7,16.7   l16.7-16.7c1.2-1.2,3.2-1.2,4.4,0s1.2,3.2,0,4.4L-496.6-203z" />
+  </CloseIconWrapper>;
+};
--- src/index.js
+++ src/index.js
@@ -1,18 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
-import PrimaryButton, {
+import { ThemeProvider } from "styled-components";
+import {
+  PrimaryButton,
   SecondaryButton,
   TertiaryButton,
-} from "./components/Buttons";
-import { GlobalStyle } from "./utils";
+  SignUpModal,
+} from "./components";
+import { GlobalStyle, darkTheme, defaultTheme } from "./utils";
 
-const App = () => (
-  <div>
-    <PrimaryButton modifiers="small">Hello World!</PrimaryButton>
-    <SecondaryButton modifiers={["large"]}>Hey World!</SecondaryButton>
-    <TertiaryButton disabled>Oh no, World!</TertiaryButton>
-    <GlobalStyle></GlobalStyle>
-  </div>
-);
+const App = () => {
+  const [useDarkTheme, setUseDarkTheme] = useState(false);
+  return (
+    <ThemeProvider theme={useDarkTheme ? darkTheme : defaultTheme}>
+      <button
+        style={{ margin: "0 16px 24px", padding: "8px", background: "none" }}
+        onClick={() => setUseDarkTheme(true)}
+      >
+        Dark Theme
+      </button>
+      <button
+        style={{ margin: "0 16px 24px", padding: "8px", background: "none" }}
+        onClick={() => setUseDarkTheme(false)}
+      >
+        Default Theme
+      </button>
+      <div
+        style={{
+          background: useDarkTheme
+            ? defaultTheme.primaryColor
+            : darkTheme.primaryColor,
+          width: "100vw",
+          height: "100vh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-around",
+        }}
+      >
+        <PrimaryButton>Hello world</PrimaryButton>
+        <SecondaryButton>Goodbye world</SecondaryButton>
+        <TertiaryButton>Hey world</TertiaryButton>
+        <SignUpModal></SignUpModal>
+        <GlobalStyle />
+      </div>
+    </ThemeProvider>
+  );
+};
 
 ReactDOM.render(<App></App>, document.querySelector("#root"));
